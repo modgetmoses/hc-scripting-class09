28c2197f0b24c040a4b6e88dd00f6a068c922a69
modgetmoses <modgetmoses@gmail.com>
2023-10-06 21:08:31 +0100

    minor changes 1

--- script.sh
+++ script.sh
@@ -8,7 +8,7 @@ webapp_NAME=my-web-app$RANDOM
 deployment_source=https://github.com/Azure-Samples/html-docs-hello-world
 Location=UKSouth
 # Create an App Service Plan
-az appservice plan create --name $APPSERVICE_PLAN_NAME --resource-group $Resource_Group_Name --location $Location --SKU f1
+az appservice plan create --name $APPSERVICE_PLAN_NAME --resource-group $Resource_Group_Name --location $Location --sku f1
 # create a web app
 az webapp create --name $webapp_NAME --plan $APPSERVICE_PLAN_NAME --resource-group $Resource_Group_Name 
 #Deploy an app from GitHub to Azure App service
