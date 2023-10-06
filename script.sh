3331afdab6c46533a532a1cfea8d441937feb0c6
modgetmoses <modgetmoses@gmail.com>
2023-10-06 21:54:53 +0100

    working version

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 # It also deploy Webapp
 #!/bin/bash
 # Create Azure Resounces
-Resource_Group_Name=testRG
+Resource_Group_Name=testRG1
 APPSERVICE_PLAN_NAME=my-app-plan$RANDOM
 WEBAPP_NAME=modgetweb$RANDOM
 deployment_source=https://github.com/Azure-Samples/html-docs-hello-world
@@ -12,5 +12,5 @@ az appservice plan create --name $APPSERVICE_PLAN_NAME --resource-group $Resourc
 # create a web app
 az webapp create --name $WEBAPP_NAME --plan $APPSERVICE_PLAN_NAME --resource-group $Resource_Group_Name 
 #Deploy an app from GitHub to Azure App service
-# az webapp deployment source config --repo-url $deployment_source --branch master --manual-integration -name $WEBAPP_NAME --resource-group $Resource_Group_Name
-az webapp deployment source config --branch master --manual-integration --name MyWebApp --repo-url https://github.com/Azure-Samples/function-image-upload-resize --resource-group $Resource_Group_Name
+az webapp deployment source config --branch master --manual-integration --name $WEBAPP_NAME --repo-url $deployment_source --resource-group $Resource_Group_Name
+# az webapp deployment source config --branch master --manual-integration --name $WEBAPP_NAME --repo-url $deployment_source --resource-group $Resource_Group_Name
